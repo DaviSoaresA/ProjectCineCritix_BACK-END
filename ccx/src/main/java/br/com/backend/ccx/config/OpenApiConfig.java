a52a42602f27dec5ebbad5bbc0d48d7d2a276c71
DaviSoaresA <didequinho@gmail.com>
2025-02-06 12:10:38 -0300

    Swagger

--- ccx/src/main/java/br/com/backend/ccx/config/OpenApiConfig.java
+++ ccx/src/main/java/br/com/backend/ccx/config/OpenApiConfig.java
@@ -1,8 +1,57 @@
 package br.com.backend.ccx.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${prop.swagger.url}")
+    private String url;
+
+    @Bean
+    public OpenAPI myOpenAPI(){
+
+        Server server = new Server();
+        server.setUrl(url);
+        server.setDescription("Servidor de Desenvolvimento");
+
+        License license = new License();
+        license.setName("Apache 2.0");
+        license.setUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+
+        Info info = new Info();
+        info.setTitle("CCX");
+		info.setVersion("0.0.1");
+		info.setDescription("CineCritix");
+		info.setLicense(license);
+		info.termsOfService("http://swagger.io/terms/");
+
+        return new OpenAPI().info(info).servers(List.of(server));
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.addAllowedOrigin("*");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+
+            source.registerCorsConfiguration("/v3/api-docs", config);
+            return new CorsFilter(source);
+    }
 }
--- ccx/src/main/java/br/com/backend/ccx/controller/PublicationController.java
+++ ccx/src/main/java/br/com/backend/ccx/controller/PublicationController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.backend.ccx.dto.PublicationDTO;
 import br.com.backend.ccx.dto.PublicationInsertDTO;
 import br.com.backend.ccx.service.PublicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/publication")
@@ -24,28 +27,59 @@ public class PublicationController {
 	@Autowired
 	private PublicationService publicationService;
 
+	@Operation(summary = "Lista todas as publicações do sistema")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Publicações localizadas e listadas com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida"),
+			@ApiResponse(responseCode = "403", description = "Operação proibida e não pode ser concluída"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@GetMapping
 	public ResponseEntity<List<PublicationDTO>> listAll() {
 		return ResponseEntity.ok(publicationService.listAll());
 	}
 
+	@Operation(summary = "Pega uma publicação no sistema através do seu id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Publicação localizada e pega com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida"),
+			@ApiResponse(responseCode = "403", description = "Operação proibida e não pode ser concluída"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@GetMapping("/{id}")
 	public ResponseEntity<PublicationDTO> searchById(@PathVariable Long id) {
 		return ResponseEntity.ok(publicationService.findById(id));
 	}
 
+	@Operation(summary = "Cadastra uma publicação no sistema")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Publicação cadastrada com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida"),
+			@ApiResponse(responseCode = "403", description = "Operação proibida e não pode ser concluída"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@PostMapping
 	public ResponseEntity<PublicationDTO> create(PublicationInsertDTO publicationInsert,
 			@RequestHeader("Authorization") String bearerToken) {
 		return ResponseEntity.ok(publicationService.save(publicationInsert, bearerToken));
 	}
 	
+	@Operation(summary = "Atualiza a Publicação através do seu id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Publicação localizada e atualizada com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida"),
+			@ApiResponse(responseCode = "403", description = "Operação proibida e não pode ser concluída"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@PutMapping("/{id}")
 	public ResponseEntity<PublicationDTO> update(@PathVariable Long id, PublicationInsertDTO publicationInsert,
             @RequestHeader("Authorization") String bearerToken) {
         return ResponseEntity.ok(publicationService.update(id, publicationInsert, bearerToken));
     }
 	
+	@Operation(summary = "Deleta uma publicação no sistema através do seu id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Publicação localizada e deletada com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida"),
+			@ApiResponse(responseCode = "403", description = "Operação proibida e não pode ser concluída"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String>delete(@PathVariable Long id){
 		return ResponseEntity.ok(publicationService.delete(id));
