d15cce3449f69f4829b2a832ea4c25fbc50e6396
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2025-02-19 11:53:48 -0300

    searching series

---
+++ ccx/src/main/java/br/com/backend/ccx/dto/SeriesInsertDTO.java
@@ -0,0 +1,54 @@
+package br.com.backend.ccx.dto;
+
+public class SeriesInsertDTO {
+
+	private String title;
+	private String year;
+	private String type;
+	private String poster;
+
+	public SeriesInsertDTO() {
+		super();
+	}
+
+	public SeriesInsertDTO(String title, String year, String type, String poster) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.type = type;
+		this.poster = poster;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+}
